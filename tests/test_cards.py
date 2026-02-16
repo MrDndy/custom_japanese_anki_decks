44546217844baaf29ba15c49c0e9295e2f32b7d0
MrDndy <guiltyspark6019@aol.com>
2026-02-15 22:05:43 -0600

    feat: add deck naming and bidirectional card mapping

---
+++ tests/test_cards.py
@@ -0,0 +1,12 @@
+﻿from jp_anki_builder.cards import build_deck_name, build_bidirectional_fields
+
+
+def test_build_deck_name_hierarchical():
+    assert build_deck_name("MangaA", "02", "07") == "MangaA::Volume02::Chapter07"
+
+
+def test_build_bidirectional_fields():
+    notes = build_bidirectional_fields(word="冒険", reading="ぼうけん", meanings=["adventure"])
+    assert len(notes) == 2
+    assert notes[0]["front"] == "冒険<br>ぼうけん"
+    assert notes[1]["front"] == "adventure"
